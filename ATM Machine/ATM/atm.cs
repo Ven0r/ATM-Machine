7de02a0785cb7b90632515340b91ea4f67d371df
Venor <venatorl4w@gmail.com>
2021-11-17 19:07:02 -0600

    updated files and workflow

--- ATM Machine/ATM/atm.cs
+++ ATM Machine/ATM/atm.cs
@@ -1,10 +1,43 @@
 using System;
 using DB;
+using Display;
 
 namespace ATM
 {
     // Transaction class that handles all the basic functions of the atm.
     // Withdraw, Deposit, Check Balance.
+    
+    public class Start
+    {
+       public static void Run()
+       {
+           Screen.Banner();
+
+           // Grab key pressed
+           ConsoleKeyInfo keyPressed = Console.ReadKey();
+           
+           // Check the key pressed
+           
+
+
+
+           
+           // Check the key pressed
+           //if (keyPressed.Key == ConsoleKey.Enter)
+           //{
+           //    Console.WriteLine("You Pressed Enter");
+           //}
+           //else if (keyPressed.Key == ConsoleKey.UpArrow)
+           //{
+           //    Console.WriteLine("Your Pressed the up arrow");
+           //}
+           //
+           //Console.WriteLine("Press any key to exit");
+           //Console.ReadKey(false);
+           
+           
+       }
+    }
     public class Transaction{
            public static int Withdraw(){
                return 0;
--- ATM Machine/Display/Display.cs
+++ ATM Machine/Display/Display.cs
@@ -4,58 +4,19 @@ namespace Display
 {
     public class Screen
     {
-        static public void Start(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("1. Login");
-                Console.WriteLine("9. Exit");
-        }
-        static public void Login(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("User Name and Pin");
-        }
-        static public void Transation(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("1. WithDraw");
-                Console.WriteLine("2. Deposit");
-                Console.WriteLine("3. See Balance");
-                Console.WriteLine("9. Logout");
-        }
-        static public void Withdraw(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("Enter Withdraw Amount");
-        }
-        static public void Deposit(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("Enter Deposit Amount");
-        }
-        static public void Balance(){
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("------= ATM MACHINE =-----");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("Your Balance is: ");
+        private int SelectedIndex;
+        private string Prompt;
+        private string[] menu = {"Login","Check Balance","Deposit","Withdraw","Exit"};
+
+        static public void Banner(){
+                Console.WriteLine(@"   
+                                       █████╗ ████████╗███╗   ███╗    ███╗   ███╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗███████╗
+                                      ██╔══██╗╚══██╔══╝████╗ ████║    ████╗ ████║██╔══██╗██╔════╝██║  ██║██║████╗  ██║██╔════╝
+                                      ███████║   ██║   ██╔████╔██║    ██╔████╔██║███████║██║     ███████║██║██╔██╗ ██║█████╗  
+                                      ██╔══██║   ██║   ██║╚██╔╝██║    ██║╚██╔╝██║██╔══██║██║     ██╔══██║██║██║╚██╗██║██╔══╝  
+                                      ██║  ██║   ██║   ██║ ╚═╝ ██║    ██║ ╚═╝ ██║██║  ██║╚██████╗██║  ██║██║██║ ╚████║███████╗
+                                      ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝    ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝
+                                                                                        ");
         }
     }
 }
-
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("------= ATM MACHINE =-----");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("1. Login");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
-//Console.WriteLine("--------------------------");
